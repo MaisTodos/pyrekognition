f6dfedeee7c67cfafcb1ecf690c35bd5dfee7421
Isabela Nascimento <iaman.isabela@gmail.com>
2022-07-20 19:07:18 -0300

    Adjusting Docker-compose and creating methods

--- pyrekognition/helper.py
+++ pyrekognition/helper.py
@@ -16,7 +16,21 @@ class HelperBotoClient:
     def _convert_url_to_bytes(self, url):
         return requests.get(url).content
 
-    def detect_labels(self, url):
+    def detect_labels(self, url, min_confidence=55):
         return rekognition_client.detect_labels(
-            Image={"Bytes": self._convert_url_to_bytes(url)}, MaxLabels=123,
+            Image={"Bytes": self._convert_url_to_bytes(url)},
+            MaxLabels=123,
+            MinConfidence=min_confidence,
         )
+
+
+class KYCValidate(HelperBotoClient):
+    def validate_label_in_document(self, url, label_name, min_confidence):
+        response = self.detect_labels(url, min_confidence)
+        return label_name in [label["Name"] for label in response["Labels"]]
+
+    def validate_id_cards(self, url, min_confidence=95):
+        return self.validate_label_in_document(url, "Id Cards", min_confidence)
+
+    def validate_driver_license(self, url, min_confidence=95):
+        return self.validate_label_in_document(url, "Driving License", min_confidence)
