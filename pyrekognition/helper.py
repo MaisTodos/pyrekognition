802c9f9e02fc330ee49a98deca4d7b179dbffa1c
Guilherme Tavares <zetavares.rib@gmail.com>
2022-08-16 11:46:31 -0300

    fix

--- pyrekognition/helper.py
+++ pyrekognition/helper.py
@@ -3,6 +3,7 @@ import requests
 
 from pyrekognition.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
+
 rekognition_client = boto3.client(
     "rekognition",
     aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -15,7 +16,18 @@ class HelperBotoClient:
 
     def _convert_url_to_bytes(self, url):
         return requests.get(url).content
-
+    
+    def compare_faces(self, url_origin, url_compare):
+        return rekognition_client.compare_faces(
+            SourceImage={'Bytes': self._convert_url_to_bytes(url_origin)},
+            TargetImage={'Bytes': self._convert_url_to_bytes(url_compare)},
+        )
+    
+    def detect_text(self, url):
+        return rekognition_client.detect_text(
+            Image={'Bytes': self._convert_url_to_bytes(url)}
+        )
+        
     def detect_labels(self, url, min_confidence=55):
         return rekognition_client.detect_labels(
             Image={"Bytes": self._convert_url_to_bytes(url)},
@@ -29,8 +41,17 @@ class KYCValidate(HelperBotoClient):
         response = self.detect_labels(url, min_confidence)
         return label_name in [label["Name"] for label in response["Labels"]]
 
+    def extract_driver_license_data(self, url):
+        return  [item['DetectedText'] for item in self.detect_text(url)["TextDetections"]]
+
     def validate_id_cards(self, url, min_confidence=95):
         return self.validate_label_in_document(url, "Id Cards", min_confidence)
 
     def validate_driver_license(self, url, min_confidence=95):
         return self.validate_label_in_document(url, "Driving License", min_confidence)
+
+    def validade_faces(self, image_source, image_target, min_confidence=98):
+        try:
+            return self.compare_faces(image_source, image_target)["FaceMatches"][0]["Similarity"] > min_confidence
+        except (KeyError, IndexError):
+            return False
--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,13 +1,16 @@
+import pytest
 from unittest.mock import patch
 
 from pyrekognition.helper import HelperBotoClient, KYCValidate
 
 
+@pytest.mark.vcr
 def test_helper_convert_url_to_bytes():
     url_ = "https://guiadocumentos.com.br/wp-content/uploads/2013/10/RIC-2010-F-310x165.jpg"  # noqa E501
     assert HelperBotoClient()._convert_url_to_bytes(url_) is not None
 
 
+@pytest.mark.vcr
 @patch("pyrekognition.helper.rekognition_client.detect_labels")
 def test_detect_labels(mock_detect_labels, aws_detect_labels):
     mock_detect_labels.return_value = mock_detect_labels
@@ -15,6 +18,22 @@ def test_detect_labels(mock_detect_labels, aws_detect_labels):
     assert HelperBotoClient().detect_labels(url_) is not None
 
 
+@pytest.mark.vcr
+def test_detect_text():
+    url_ = "https://guiadocumentos.com.br/wp-content/uploads/2013/10/RIC-2010-F-310x165.jpg"  # noqa E501
+    response = HelperBotoClient().detect_text(url_)
+    assert response is not None
+
+
+@pytest.mark.vcr
+def test_compare_faces():
+    url_origin = "https://guiadocumentos.com.br/wp-content/uploads/2013/10/RIC-2010-F-310x165.jpg"  # noqa E501
+    url_targer = "https://gringo.com.vc/wp-content/uploads/2021/05/geral-CNH-867x1024.png"
+    response = HelperBotoClient().compare_faces(url_origin, url_targer)
+    assert response is not None
+
+
+@pytest.mark.vcr
 def test_validate_id_cards_success():
     url = "https://gringo.com.vc/wp-content/uploads/2021/05/geral-CNH-867x1024.png"
     validator = KYCValidate()
@@ -23,6 +42,7 @@ def test_validate_id_cards_success():
     assert response is True
 
 
+@pytest.mark.vcr
 def test_validate_id_cards_failed():
     url = "https://images.emojiterra.com/google/noto-emoji/v2.034/512px/1f984.png"
     validator = KYCValidate()
@@ -31,6 +51,7 @@ def test_validate_id_cards_failed():
     assert response is False
 
 
+@pytest.mark.vcr
 def test_validate_driver_license_success():
     url = "https://www.dof.ms.gov.br/wp-content/uploads/2019/08/28-CNH-falsa-em-Amambai.jpg"  # noqa E501
     validator = KYCValidate()
@@ -39,9 +60,46 @@ def test_validate_driver_license_success():
     assert response is True
 
 
+@pytest.mark.vcr
 def test_validate_driver_license_failed():
     url = "https://gringo.com.vc/wp-content/uploads/2021/05/geral-CNH-867x1024.png"
     validator = KYCValidate()
     response = validator.validate_driver_license(url)
 
     assert response is False
+
+
+@pytest.mark.vcr
+def test_validade_faces():
+    url_origin = "https://pbs.twimg.com/profile_images/1226626761611456513/MMyVfrYT_400x400.jpg"  # noqa E501
+    url_targer = "https://img.a.transfermarkt.technology/portrait/big/3366-1489607001.jpg"
+
+    validator = KYCValidate()
+    assert validator.validade_faces(url_origin, url_targer) is True
+
+
+@pytest.mark.vcr
+def test_validade_faces_not_matched():
+    url_origin = "https://pbs.twimg.com/profile_images/1226626761611456513/MMyVfrYT_400x400.jpg"  # noqa E501
+    url_targer = "https://gringo.com.vc/wp-content/uploads/2021/05/geral-CNH-867x1024.png"
+
+    validator = KYCValidate()
+    assert validator.validade_faces(url_origin, url_targer) is False
+
+
+@pytest.mark.vcr
+def test_extract_driver_license_data():
+    url = "https://gringo.com.vc/wp-content/uploads/2021/05/geral-CNH-867x1024.png"
+    validator = KYCValidate()
+    print(validator.validate_driver_license(url))
+    data = validator.extract_driver_license_data(url)
+    print(data)
+
+
+@pytest.mark.vcr
+def test_extract_driver_license_data_fake():
+    url = 'https://support-candidates.gupy.io/hc/article_attachments/4419319664283/cnh_fisica.png'
+    validator = KYCValidate()
+    print(validator.validate_driver_license(url))
+    data = validator.extract_driver_license_data(url)
+    print(data)
